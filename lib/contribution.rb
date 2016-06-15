26e436d7a6a8ab5d3f17bdc8f298f0f67e28c992
Mark Wingerd <mawingerd@gmail.com>
2016-06-14 23:15:09 -0700

    Added initial test to handle input string

--- lib/contribution.rb
+++ lib/contribution.rb
@@ -1,2 +1,4 @@
 class Contribution
+  def initialize(file_path)
+  end
 end
--- spec/contribution_spec.rb
+++ spec/contribution_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require 'contribution'
 
 describe Contribution do
-  
+  let(:contribution) { Contribution.new("specs/initial_test.csv") }
+
+  describe "#initialize" do
+    it "accepts a string as an argument" do
+      expect { Contribution.new("specs/initial_test.csv") }.not_to raise_error
+    end
+  end
 end
